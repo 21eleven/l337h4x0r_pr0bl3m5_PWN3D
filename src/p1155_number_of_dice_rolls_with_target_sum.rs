8a9724514fd097910cca7e9254f5c595b7c0fe1e
Noah Lidell <noahlidell@gmail.com>
2024-01-07 22:58:45 -0600

    p1155 in the midst of troubleshooting this one

--- src/p1155_number_of_dice_rolls_with_target_sum.rs
+++ src/p1155_number_of_dice_rolls_with_target_sum.rs
@@ -1,116 +1,121 @@
-use std::{u128, collections::HashMap};
+use std::{collections::HashMap, u128};
 
-// 
+//
 // [1155] Number of Dice Rolls With Target Sum
-// 
-// 
+//
+//
 // You have n dice, and each die has k faces numbered from 1 to k.
-// 
+//
 // Given three integers n, k, and target, return the number of possible ways (out of the kn total ways) to roll the dice, so the sum of the face-up numbers equals target. Since the answer may be too large, return it modulo 10⁹ + 7.
-// 
-//  
+//
+//
 // Example 1:
-// 
+//
 // Input: n = 1, k = 6, target = 3
 // Output: 1
 // Explanation: You throw one die with 6 faces.
 // There is only one way to get a sum of 3.
-// 
-// 
+//
+//
 // Example 2:
-// 
+//
 // Input: n = 2, k = 6, target = 7
 // Output: 6
 // Explanation: You throw two dice, each with 6 faces.
 // There are 6 ways to get a sum of 7: 1+6, 2+5, 3+4, 4+3, 5+2, 6+1.
-// 
-// 
+//
+//
 // Example 3:
-// 
+//
 // Input: n = 30, k = 30, target = 500
 // Output: 222616187
 // Explanation: The answer must be returned modulo 10⁹ + 7.
-// 
-// 
-//  
+//
+//
+//
 // Constraints:
-// 
-// 
+//
+//
 // 	1 <= n, k <= 30
 // 	1 <= target <= 1000
-// 
-// 
+//
+//
 struct Solution;
-    const  MOD : i32 = 10_i32.pow(9_u32) + 7;
-    // int MOD = 1000000000 + 7;
-    // Map<String, Integer> memo = new HashMap<>();
-    // public int numRollsToTarget(int d, int f, int target) {
-    //     if (d == 0 && target == 0) {
-    //         return 1;
-    //     }
-    //     if (d == 0 || target == 0) {
-    //         return 0;
-    //     }
-    //     String str = d + " " + target;
-    //     if (memo.containsKey(str)) {
-    //         return memo.get(str);
-    //     }
-    //     int res = 0;
-    //     for (int i = 1; i <= f; i++) {
-    //         if (target >= i) {
-    //             res = (res + numRollsToTarget(d - 1, f, target - i)) % MOD;
-    //         } else {
-    //             break;
-    //         }
-    //     }
-    //     memo.put(str, res);
-    //     return res;
-    // }
+const MOD: i32 = 10_i32.pow(9_u32) + 7;
 impl Solution {
-    pub fn num_rolls_to_target(n: i32, k: i32, target: i32) -> i32 {
-        let mut memo: HashMap<(i32, i32), i32> = HashMap::new(); 
-        fn recurse(val: i32, rolls: i32, k: i32, memo: &mut HashMap<(i32, i32), i32>) -> i32 {
-            if val == 0 && rolls == 0 {
-                1
-            } else if val < 0 || rolls <= 0 {
-                // dbg!("hit bottom: {} {}", val, rolls);
-                0
-            }
-            else if memo.contains_key(&(val, rolls)) {
-                *memo.get(&(val, rolls)).unwrap()
-            }
-            else {
-                let mut res = 0;
-                for x in 1..=k {
-                    if val >= x {
-                        res += recurse(val - x, rolls - 1, k, memo) % MOD;
-                    } else {
-                        break
-                    }
+    // pub fn num_rolls_to_target(n: i32, k: i32, target: i32) -> i32 {
+    //     // n is the number of die you have
+    //     // k is the faces on the die
+    //     if target < n {
+    //         return 0;
+    //     } // if you have 10 dice and you have
+    //       // a target of 5 then there is no way
+    //       // to get a score of less then 10
+    //       //
+    //     if target >= n * k {
+    //         return 0;
+    //     } // target too big
+    //
+    //     let mut n_ways: [i32; 1001] = [0; 1001];
+    //     // first roll of the die
+    //     // 1 way to roll a number from 1 to
+    //     for j in 1..=k {
+    //         n_ways[j as usize] = 1;
+    //     }
+    //
+    //     // do the n die rolls
+    //     // but start at one since we've already simulated the first one
+    //     for i in 2..=n {
+    //         let mut tmp: [i32; 1001] = [0; 1001];
+    //         for j in 1..=k {
+    //             // operate on a range of value
+    //             let a = (target - (n - i) * k - j).max(0);
+    //             let b = (target - (n - i) - j).max(0);
+    //             for idx in a..=b {
+    //                 tmp[(idx+j) as usize] = (tmp[(idx+j) as usize] + n_ways[idx as usize]) % MOD;
+    //             }
+    //         }
+    //         n_ways = tmp;
+    //     }
+    //     n_ways[target as usize]
+    // }
+    pub fn num_rolls_to_target(n: i32, k: i32, target: i32) -> i32 
+    {
+        // [1] trivial cases
+        if target > n*k { return 0; }
+        if target < n   { return 0; }
+        
+        // [2] first roll of the die (i.e., 1 way to roll a number from 1 to k)
+        let mut n_of_ways: [i32;1001] = [0;1001];
+        for j in 1..=k { n_of_ways[j as usize] = 1; }
+        
+        // [3] iterate over subsequent rolls 
+        for i in 2..=n
+        {
+            let mut n_of_ways_: [i32;1001] = [0;1001];
+            for j in 1..=k
+            {
+                // [4] for each value of face-up number we care only 
+                //     about a certain range of sums that allows us  
+                //     to finally reach a target value
+                let beg = (target - (n-i)*k - j).max(0);
+                let end = (target - (n-i) - j).max(0);
+                
+                for s in beg..=end
+                {
+                    n_of_ways_[(s+j) as usize] = 
+                        (n_of_ways_[(s+j) as usize] + n_of_ways[s as usize]) % 1000000007;
                 }
-                let _ = memo.insert((val, rolls), res);
-                res
             }
-            // else {
-            //     match memo.get(&(val, rolls)) {
-            //         Some(n_ways) => *n_ways,
-            //         None => {
-            //             // let mut n_ways = 0;
-            //             // for x in 1..=k {
-            //             //     n_ways += 
-            //             //
-            //             // }
-            //             let n_ways: i32 = (1..=k).into_iter()
-            //                 .map(|roll| recurse(val-roll, rolls-1, k, memo) )
-            //                 .sum();
-            //             let _ = memo.insert((val, rolls), n_ways);
-            //             n_ways % MOD
-            //         }
-            //     }
-            // }
-            // todo!();
+            
+            // [5] update the number of ways to reach calculated
+            //     sums after i-th roll of the dice
+            n_of_ways = n_of_ways_;
         }
-        recurse(target, n, k, &mut memo) 
+
+        // [6] among calculated values, there is a corresponding
+        //     number of ways for 'target', just return it
+        n_of_ways[target as usize]
     }
 }
 
@@ -133,5 +138,4 @@ fn big_input() {
     let res = Solution::num_rolls_to_target(30, 30, 500);
     dbg!(res);
     assert!(res == 222616187);
-
 }
