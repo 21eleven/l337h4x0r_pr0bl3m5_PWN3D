4a8233b822c5df92c052782681ebd2e3942e8d0a
Noah Lidell <noahlidell@gmail.com>
2024-01-07 22:22:06 -0600

    naive greedy p121

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,27 @@ mod p0001_two_sum;
 mod p0021_merge_two_sorted_lists;
 mod p1662_check_if_two_string_arrays_are_equivalent;
 mod p1155_number_of_dice_rolls_with_target_sum;
+mod p0938_range_sum_of_bst;
+mod p0121_best_time_to_buy_and_sell_stock;
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+  pub val: i32,
+  pub left: Option<Rc<RefCell<TreeNode>>>,
+  pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+  #[inline]
+  pub fn new(val: i32) -> Self {
+    TreeNode {
+      val,
+      left: None,
+      right: None
+    }
+  }
+}
+
